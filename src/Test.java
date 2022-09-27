a93a5be0568cbe2f26f1186ac2225b36b8fb5af3
jerrysalieri <112954102+jerrysalieri@users.noreply.github.com>
2022-09-27 10:22:21 +0200

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Test {
+import java.util.Scanner;
+public class Test {
+    public static String inputData(){
+        System.out.println("Input:");
+        Scanner scan = new Scanner(System.in);
+        String data = scan.nextLine();
+
+        return data;
+    }
+    public static void myMethod(){
+        System.out.print("I just used a method without return value");
+    }
+    public static int Number(){
+
+        int test;
+        test =Integer.parseInt(inputData());
+        return test;
+    }
+    public static void main(String []args){
+        System.out.println(Number());
+       myMethod();
+    }
 }
